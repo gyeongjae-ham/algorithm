b17d2bbcee02df0e069e69f86ea6f247c5002d7b
hiyee-gj <gyeongjae.h.dev@gmail.com>
2022-08-12 14:40:58 +0900

    :sparkles: Add vector parameter로 넘겨서 수정하기

---
+++ meterials/Data structure/07-vector_param_2.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void f(vector<int> v[10]) { v[0][0] = 1000; }
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  vector<int> v[10];
+  v[0].push_back(100);
+
+  f(v);
+
+  for (int i : v[0]) cout << i << " ";
+
+  return 0;
+}
