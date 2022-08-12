0142a25c0a76d8a081320b5b1393946847390376
hiyee-gj <gyeongjae.h.dev@gmail.com>
2022-08-12 17:35:57 +0900

    :sparkles: Add queue

---
+++ meterials/Data structure/17-queue.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+//* 큐는 먼저 집어넣은 데이터가 먼저 나오는 자료구조
+//* 나중에 넣은 데이터가 먼저 나오는 스택의 반대
+//* 삽입, 삭제에 O(1), 탐색에 O(N)
+//* 주로 BFS에 쓰인다. 선입선출구조(FIFO)로써 사용됨
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  queue<int> q;
+  q.push(1);
+  cout << q.front() << "\n";
+
+  q.pop();
+  cout << q.size() << "\n";
+
+  return 0;
+}
---
+++ meterials/Data structure/18-queue_2.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+//* 보통 queue는 아래와 같이 size()와 같이 쓰임
+//* pop을 하게 되면 size가 작아지고 0ㅇ 되면 while문 종료
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  queue<int> q;
+  for (int i = 1; i <= 10; i++) q.push(i);
+
+  while (q.size()) {
+    cout << q.front() << "\n";
+    q.pop();
+  }
+
+  return 0;
+}
