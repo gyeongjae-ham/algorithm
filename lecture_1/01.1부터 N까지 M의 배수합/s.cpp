3ca6b89499640edc85db4cede4aeca0e5635284c
hiyee-gj <gyeongjae.h.dev@gmail.com>
2022-08-16 09:54:48 +0900

    :sparkles: Add 01.1부터 N까지 M의 배수합

---
+++ lecture_1/01.1부터 N까지 M의 배수합/s.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int n, m, answer;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  cin >> n >> m;
+
+  for (int i = 1; i <= n; i++) {
+    if (i % m == 0) {
+      answer += i;
+    }
+  }
+
+  cout << answer << "\n";
+
+  return 0;
+}
