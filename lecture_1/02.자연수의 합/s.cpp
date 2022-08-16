a1dfe955efd5da81d48029ed3aa9d10b398c59c6
hiyee-gj <gyeongjae.h.dev@gmail.com>
2022-08-16 10:10:17 +0900

    :sparkles: Add 02. 자연수의 합

---
+++ lecture_1/02.자연수의 합/s.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int a, b, sum;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  cin >> a >> b;
+
+  for (int i = a; i <= b; i++) {
+    sum += i;
+    if (i == b)
+      cout << i << " = " << sum << "\n";
+    else
+      cout << i << " + ";
+  }
+
+  return 0;
+}
