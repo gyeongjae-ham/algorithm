605aaa0c0ef45474b5657d235d6b4d8d022ed910
hiyee-gj <gyeongjae.h.dev@gmail.com>
2022-08-16 17:43:36 +0900

    :sparkles: Add 영어단어 복구

---
+++ lecture_1/07.영어단어 복구/s.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+string s;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+  getline(cin, s);
+
+  s.erase(remove(s.begin(), s.end(), ' '), s.end());
+  transform(s.begin(), s.end(), s.begin(), ::tolower);
+
+  cout << s << "\n";
+
+  return 0;
+}
