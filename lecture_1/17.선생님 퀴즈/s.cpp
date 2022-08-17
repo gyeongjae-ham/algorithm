52f0b8372597de6354fc74077ce90b2a70fad23b
hiyee-gj <gyeongjae.h.dev@gmail.com>
2022-08-17 23:56:23 +0900

    :sparkles: Add 17.선생님 퀴즈

---
+++ lecture_1/17.선생님 퀴즈/s.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int s, n, tmp;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  cin >> s;
+  for (int i = 0; i < s; i++) {
+    cin >> n >> tmp;
+    int res = 0;
+    for (int j = 1; j <= n; j++) {
+      res += j;
+    }
+    if (tmp == res)
+      cout << "YES"
+           << "\n";
+    else
+      cout << "NO"
+           << "\n";
+  }
+
+  return 0;
+}
