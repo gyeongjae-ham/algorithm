9f6d78156b8c82470a1c7e89a82062b568c54215
hiyee-gj <gyeongjae.h.dev@gmail.com>
2022-08-18 09:28:22 +0900

    :sparkles: Add 18.층간소음

---
+++ lecture_1/18.층간소음/s.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int n, l, t, cnt = 0;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  cin >> n >> l;
+  int max = -2147000000;
+  for (int i = 0; i < n; i++) {
+    cin >> t;
+    if (t <= 90 && cnt == 0) {
+      continue;
+    } else if (t <= 90 && cnt != 0) {
+      if (max < cnt) {
+        max = cnt;
+        cnt = 0;
+      }
+    } else if (t > 90) {
+      cnt++;
+    }
+  }
+
+  cout << max << "\n";
+
+  return 0;
+}
