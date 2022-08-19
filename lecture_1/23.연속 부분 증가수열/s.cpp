47d0d8750b1bc757f7ddae1847948ed346482a9c
hiyee-gj <gyeongjae.h.dev@gmail.com>
2022-08-19 14:02:34 +0900

    :recycle: 23.연속 부분 증가수열
    풀이 작성

--- lecture_1/23.연속 부분 증가수열/s.cpp
+++ lecture_1/23.연속 부분 증가수열/s.cpp
@@ -1,9 +1,33 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+int n, num, pre, now;
 
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(NULL);
   cout.tie(NULL);
+
+  cin >> n;
+  cin >> pre;
+
+  int cnt = 1;
+  int max = 1;
+  for (int i = 0; i < n - 1; i++) {
+    cin >> num;
+    if (pre <= num) {
+      cnt++;
+    } else {
+      if (cnt > max) {
+        max = cnt;
+      }
+      cnt = 1;
+    }
+
+    pre = num;
+  }
+
+  cout << max << "\n";
+
+  return 0;
 }
