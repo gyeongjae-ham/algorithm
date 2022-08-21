e268a9efb3be6c5017256decb3e138432e4ac637
hiyee-gj <gyeongjae.h.dev@gmail.com>
2022-08-21 17:34:51 +0900

    :sparkles: Add 25.석차 구하기

---
+++ lecture_1/25.석차 구하기/s.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int n, num, a[200], b[200];
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  cin >> n;
+
+  for (int i = 0; i < n; i++) {
+    cin >> num;
+    a[i] = num;
+    b[i] = 1;
+  }
+
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      if (a[j] > a[i]) b[i]++;
+    }
+  }
+
+  for (int i = 0; i < n; i++) {
+    cout << b[i] << " ";
+  }
+  cout << "\n";
+
+  return 0;
+}
