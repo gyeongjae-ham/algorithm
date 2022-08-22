74625a19ac5f6da4b27d8cc798b2ce267deaa135
hiyee-gj <gyeongjae.h.dev@gmail.com>
2022-08-22 13:40:41 +0900

    :sparkles: Add 32.선택정렬

---
+++ lecture_1/32.선택정렬/s.cpp
@@ -0,0 +1,31 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  int n, num, a[104], tmp;
+
+  scanf("%d", &n);
+  for (int i = 0; i < n; i++) {
+    scanf("%d", &a[i]);
+  }
+
+  for (int i = 0; i < n; i++) {
+    int idx = i;
+    for (int j = i + 1; j < n; j++) {
+      if (a[j] < a[idx]) idx = j;
+    }
+    tmp = a[i];
+    a[i] = a[idx];
+    a[idx] = tmp;
+  }
+
+  for (int i = 0; i < n; i++) {
+    printf("%d ", a[i]);
+  }
+  printf("\n");
+
+  return 0;
+}
